9e5f4e4d587229358c5eed2d31f744e2b4f0eede
dharmesh <darmesh@iqminds.org>
2021-12-29 10:47:15 +0530

    [ADD]code added for changes

--- crystal_reservation/models/reservation.py
+++ crystal_reservation/models/reservation.py
@@ -6,24 +6,25 @@ from datetime import datetime,timedelta
 
 class crystal_reservation(models.Model):
     _name = 'reservation.reservation'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = 'seq'
 
-    seq = fields.Char("Reservation Number", default="New")
-    first_name = fields.Char("First Name")
-    lastname = fields.Char("Last Name")
+    seq = fields.Char("Reservation Number", default="New", tracking=True)
+    first_name = fields.Char("First Name", tracking=True)
+    lastname = fields.Char("Last Name", tracking=True)
     calendar = fields.Datetime("Calendar")
-    reservation_date = fields.Date("Date")
+    reservation_date = fields.Date("Date", tracking=True)
     start_date = fields.Datetime("Start Date",compute='_compute_start_date',store=True)
     end_date = fields.Datetime("End Date",compute='_compute_start_date',store=True)
     full_name = fields.Char("Name",compute='_compute_full_name',store=True)
-    email = fields.Char("Email")
-    phone = fields.Char("Phone")
+    email = fields.Char("Email", tracking=True)
+    phone = fields.Char("Phone", tracking=True)
     no_guest = fields.Char("Number Of Guests")
     no_children = fields.Char("Number Of Children")
-    dome = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
-    no_guest_sel = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6')],string="Number Of Guests")
-    no_children_sel = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6')],string="Number Of Children")
-    time_slot = fields.Selection([('11:45', '11:45'),('14:00', '14:00'),('16:30', '16:30'),('18:45', '18:45'),('21:00', '21:00')],string="Time Slot")
+    dome = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')], tracking=True)
+    no_guest_sel = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6')],string="Number Of Guests", tracking=True)
+    no_children_sel = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6')],string="Number Of Children", tracking=True)
+    time_slot = fields.Selection([('11:45', '11:45'),('14:00', '14:00'),('16:30', '16:30'),('18:45', '18:45'),('21:00', '21:00')],string="Time Slot", tracking=True)
 
     @api.model
     def create(self, vals):
