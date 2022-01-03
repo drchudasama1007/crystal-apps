29d07dd1604cf01c0e3afc54d32867cd80ef656e
dharmesh <darmesh@iqminds.org>
2022-01-03 21:06:41 +0530

    [ADD]code added for changes

--- crystal_reservation/models/reservation.py
+++ crystal_reservation/models/reservation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from datetime import datetime,timedelta
 
 
@@ -53,3 +53,38 @@ class crystal_reservation(models.Model):
                 rec.full_name = rec.lastname
             else:
                 rec.full_name = ''
+
+    # Function For Send Mail For Proposal Submit
+    def action_send_email(self):
+        '''
+        This function opens a window to compose an email, with the emai template message loaded by default
+        '''
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('crystal_reservation', 'mail_template_reservation')[
+                1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = {
+            'default_model': 'reservation.reservation',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_document_sent': True,
+        }
+        return {
+            'name': _('Confirmation Email'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
